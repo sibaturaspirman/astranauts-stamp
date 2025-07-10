5f194128a5168a307b2311a0c8579b93453b144e
Asep Irman <asepirman@antigravity.id>
2025-07-11 04:30:52 +0700

    upd next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['antigrvty.s3.ap-southeast-1.amazonaws.com'],
+    },
+}
+  
+export default nextConfig
