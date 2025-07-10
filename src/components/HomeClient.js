23e01f1556a49cea87dec5e9cd2f56137c17ad34
Asep Irman <asepirman@antigravity.id>
2025-07-11 04:27:44 +0700

    homeclient

--- src/components/HomeClient.js
+++ src/components/HomeClient.js
@@ -10,7 +10,7 @@ export default function HomeClient({ booths, profile }) {
   return (
     <main className="w-full max-w-sm p-4 pt-0 pb-0">
         <h2 className="text-center text-sm mb-0">Hi, {profile?.name || '...'}</h2>
-        <h1 className="text-center font-bold text-lg mb-2">Let's Explore Astranauts 2025</h1>
+        <h1 className="text-center font-bold text-lg mb-2">Let&apos;s Explore Astranauts 2025</h1>
 
         <IntroSwiper hidden={showIntro} onFinished={() => setShowIntro(true)} />
         <div className="w-full">
