49f88d022b4a09d4d6f179d9575af228362111b6
Asep Irman <asepirman@antigravity.id>
2025-07-10 23:10:42 +0700

    upd swiper

--- src/components/SwiperBooth.js
+++ src/components/SwiperBooth.js
@@ -1,10 +1,12 @@
 'use client'
+import { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/pagination'
 
 export default function SwiperBooth({ booths, profile }) {
+    const [activeIndex, setActiveIndex] = useState(0)
   return (
     <div className="w-full overflow-hidden">
       <Swiper
@@ -15,8 +17,7 @@ export default function SwiperBooth({ booths, profile }) {
         className="mySwiper"
         grabCursor={true}
         spaceBetween={15}
-        onSlideChange={(swiper) => {
-        }}
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
       >
         {booths.map((booth) => (
           <SwiperSlide key={booth.id}>
@@ -30,6 +31,16 @@ export default function SwiperBooth({ booths, profile }) {
             </div>
           </SwiperSlide>
         ))}
+            <SwiperSlide key={11}>
+            <div className="rounded-3xl bg-white shadow-md overflow-hidden">
+              {/* <img
+                src={booth.memberStamps?.[0]?.imageUrl || '/placeholder.jpg'}
+                alt={booth.name}
+                className="w-full h-48 object-cover"
+              /> */}
+              <div className="text-center font-medium py-3">SHARE SOSMED</div>
+            </div>
+          </SwiperSlide>
       </Swiper>
 
       <div className="flex items-center justify-between text-xs text-gray-600 mt-4">
@@ -38,7 +49,7 @@ export default function SwiperBooth({ booths, profile }) {
           <img src="/coin.svg" className="h-4" alt="coin" />
           <span>{profile?.coins ?? 0} Coins</span>
         </div>
-        <div>{booths.length.toString().padStart(2, '0')}/11</div>
+        <div>{(activeIndex + 1).toString().padStart(2, '0')}/{(booths.length + 1).toString().padStart(2, '0')}</div>
       </div>
     </div>
   )
